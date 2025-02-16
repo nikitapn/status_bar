0ae34d0a221cc67b1e8834f61b179d814f7dc324
Nikita Pennie <nikitapnn1@gmail.com>
2025-02-16 19:40:58 +0300

    bolt

--- main.c
+++ main.c
@@ -1,41 +1,41 @@
-#include <stdlib.h>
+#include <X11/Xlib.h>
+#include <errno.h>
+#include <libudev.h>
 #include <poll.h>
+#include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
-#include <libudev.h>
-#include <X11/Xlib.h>
-#include <pthread.h>
-#include <errno.h>
 
 #define MAX_BLOCK_SIZE 128
 #define MAX_STATUS_LEN 1024
-#define LEFT_BORDER "^c#dddddd^["
-#define RIGHT_BORDER "^c#dddddd^]"
+#define LEFT_BORDER    "^c#dddddd^["
+#define RIGHT_BORDER   "^c#dddddd^]"
 
+enum Block : int { Battery, Memory, Date, MAX_BLOCKS };
 
-enum Block : int {
-  Battery, Memory, Date, MAX_BLOCKS
-};
-
-static Display *dpy;
-static int screen;
-static Window root;
+static Display        *dpy;
+static int             screen;
+static Window          root;
 static pthread_mutex_t mutex;
+static int             is_cable_plugged;
 
 struct BlockData {
-  int len;
+  int  len;
   char data[MAX_BLOCK_SIZE];
 };
 
-static void update(enum Block bix, const char* str) {
-  static char status [MAX_STATUS_LEN] = "";
-  static struct BlockData blocks[MAX_BLOCKS] = { 0 };
+static void update(
+    enum Block bix, const char *str)
+{
+  static char             status[MAX_STATUS_LEN] = "";
+  static struct BlockData blocks[MAX_BLOCKS]     = {0};
 
   pthread_mutex_lock(&mutex);
 
-  const int blen = strlen(str);
+  const int blen  = strlen(str);
   blocks[bix].len = blen;
   memcpy(blocks[bix].data, str, blen);
 
@@ -57,45 +57,45 @@ static void update(enum Block bix, const char* str) {
   XFlush(dpy);
 }
 
-static char months[][4] = {
-  "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
-}; 
+static char months[][4] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
 
-static char days[][4] = {
-  "Sun", "Mon", "Tue", "Wen", "Thu", "Fri", "Sat"
-}; 
+static char days[][4] = {"Sun", "Mon", "Tue", "Wen", "Thu", "Fri", "Sat"};
 
-static void* time_thread(void* /* params */) {
+static void *time_thread(
+    void * /* params */)
+{
   for (;;) {
     static char str[MAX_BLOCK_SIZE];
-    time_t tm = time(NULL);
-    struct tm *date = localtime(&tm);
-    sprintf(str, " ^c#c7d7e8^\uf017  ^c#bbbbbb^%s %s %d %s %.02d:%.02d ",
-      days[date->tm_wday], 
-      months[date->tm_mon],
-      date->tm_mday,
-      (date->tm_hour >= 8 && date->tm_hour < 21 ? "^c#edd238^\uf185" : "^c#ecede8^\uf186"),
-      date->tm_hour, 
-      date->tm_min
-    );
+    time_t      tm   = time(NULL);
+    struct tm  *date = localtime(&tm);
+    sprintf(str,
+            " ^c#c7d7e8^\uf017  ^c#bbbbbb^%s %s %d %s %.02d:%.02d ",
+            days[date->tm_wday],
+            months[date->tm_mon],
+            date->tm_mday,
+            (date->tm_hour >= 8 && date->tm_hour < 21 ? "^c#edd238^\uf185" : "^c#ecede8^\uf186"),
+            date->tm_hour,
+            date->tm_min);
 
     update(Date, str);
-    
+
     sleep(60 - date->tm_sec + 1);
   }
 
   return NULL;
 }
 
-static const char battery_icon [][16] = { 
-  "^c#ff0000^ \uf244 ",
-  "^c#eb9634^ \uf243 ",
-  "^c#ebd334^ \uf242 ",
-  "^c#c6eb34^ \uf241 ",
-  "^c#00ff00^ \uf240 ",
+static const char battery_icon[][16] = {
+    "^c#ff0000^ \uf244 ",
+    "^c#eb9634^ \uf243 ",
+    "^c#ebd334^ \uf242 ",
+    "^c#c6eb34^ \uf241 ",
+    "^c#00ff00^ \uf240 ",
 };
 
-static const char* get_battery_icon(const char* str) {
+static const char *get_battery_icon(
+    const char *str)
+{
   int value = atoi(str);
   if (value < 10) return battery_icon[0];
   if (value >= 10 && value < 25) return battery_icon[1];
@@ -104,25 +104,24 @@ static const char* get_battery_icon(const char* str) {
   return battery_icon[4];
 }
 
-static void* memory_status(void* /* params */) {
+static void *memory_status(
+    void * /* params */)
+{
   for (;;) {
     FILE *file = fopen("/proc/meminfo", "r");
     if (file == NULL) {
       perror("fopen");
     }
 
-    char line[256];
+    char          line[256];
     unsigned long memTotal = 0, memFree = 0, buffers = 0, cached = 0, memReclaimable = 0;
 
     for (int i = 0; fgets(line, sizeof(line), file);) {
       if (i == 5) break;
-      if (
-          (sscanf(line, "MemTotal: %lu kB", &memTotal) == 1) ||
-          (sscanf(line, "MemFree: %lu kB", &memFree) == 1)   ||
-          (sscanf(line, "Buffers: %lu kB", &buffers) == 1)   ||
-          (sscanf(line, "Cached: %lu kB", &cached) == 1)     ||
-          (sscanf(line, "SReclaimable: %lu kB", &memReclaimable) == 1)
-           ) ++i;
+      if ((sscanf(line, "MemTotal: %lu kB", &memTotal) == 1) || (sscanf(line, "MemFree: %lu kB", &memFree) == 1) ||
+          (sscanf(line, "Buffers: %lu kB", &buffers) == 1) || (sscanf(line, "Cached: %lu kB", &cached) == 1) ||
+          (sscanf(line, "SReclaimable: %lu kB", &memReclaimable) == 1))
+        ++i;
     }
 
     fclose(file);
@@ -138,29 +137,51 @@ static void* memory_status(void* /* params */) {
   return NULL;
 }
 
+static const char *get_bolt(
+    const char *str)
+{
+  return strcmp(str, "Charging") == 0 ? " ^c#cccccc^\uf0e7" : "";
+}
 
-static void populate_with_initial_values() {
-  { // init battery status
+static void populate_with_initial_values(
+    void)
+{
+  {  // init battery status
     static char str[MAX_BLOCK_SIZE];
-    char capacity [5] = "---";
-    FILE* fp = fopen("/sys/class/power_supply/BAT0/capacity", "r");
+    char        capacity[5] = "---";
+    FILE       *fp          = fopen("/sys/class/power_supply/BAT0/capacity", "r");
     if (fp) {
       fgets(capacity, 5, fp);
       int len = (int)ftell(fp);
       if (len > 1) {
-        capacity[len - 1] = '\0'; 
+        capacity[len - 1] = '\0';
         memcpy(str, get_battery_icon(capacity), sizeof(battery_icon[0]) - 1);
       }
       fclose(fp);
     }
-    sprintf(str + sizeof(battery_icon[0]) - 1, " %s%%", capacity);
+    const char *charging = "";
+    {
+      FILE *fp = fopen("/sys/class/power_supply/BAT0/status", "r");
+      if (fp) {
+        char temp[64];
+        fgets(temp, 64, fp);
+        int len       = (int)ftell(fp);
+        temp[len - 1] = '\0';
+        charging      = get_bolt(temp);
+        if (strlen(charging) > 0) is_cable_plugged = 1;
+        fclose(fp);
+      }
+    }
+    sprintf(str + sizeof(battery_icon[0]) - 1, "%s%%%s", capacity, charging);
     update(Battery, str);
   }
 }
 
-static int monitor_battery() {
-  struct udev *udev;
-  struct udev_device *dev;
+static int monitor_battery(
+    void)
+{
+  struct udev         *udev;
+  struct udev_device  *dev;
   struct udev_monitor *mon;
 
   udev = udev_new();
@@ -177,10 +198,12 @@ static int monitor_battery() {
   udev_monitor_enable_receiving(mon);
 
   struct pollfd items[1];
-  items[0].fd = udev_monitor_get_fd(mon);
-  items[0].events = POLLIN;
+  items[0].fd      = udev_monitor_get_fd(mon);
+  items[0].events  = POLLIN;
   items[0].revents = 0;
 
+  const char *bolt      = "";
+  char        level[16] = "";
   while (poll(items, 1, -1) > 0) {
     dev = udev_monitor_receive_device(mon);
     if (dev) {
@@ -190,10 +213,21 @@ static int monitor_battery() {
         printf("%s: %s\n", udev_list_entry_get_name(list), udev_list_entry_get_value(list));
       }*/
       const char *capacity = udev_device_get_property_value(dev, "POWER_SUPPLY_CAPACITY");
+      const char *status   = udev_device_get_property_value(dev, "POWER_SUPPLY_STATUS");
+      const char *online   = udev_device_get_property_value(dev, "POWER_SUPPLY_ONLINE");
+      if (online) {
+        is_cable_plugged = (strcmp(online, "1") == 0);
+      }
+      if (status) {
+        bolt = !is_cable_plugged ? "" : get_bolt(status);
+      }
       if (capacity) {
+        strcpy(level, capacity);
+      }
+      if (capacity || status) {
         static char str[MAX_BLOCK_SIZE];
-        memcpy(str, get_battery_icon(capacity), sizeof(battery_icon[0]) - 1);
-        sprintf(str + sizeof(battery_icon[0]) - 1, " %s%%", capacity);
+        memcpy(str, get_battery_icon(level), sizeof(battery_icon[0]) - 1);
+        sprintf(str + sizeof(battery_icon[0]) - 1, "%s%%%s", level, bolt);
         update(Battery, str);
       }
       udev_device_unref(dev);
@@ -205,15 +239,16 @@ static int monitor_battery() {
   return EXIT_SUCCESS;
 }
 
-int main() {
+int main()
+{
   if (0 != (errno = pthread_mutex_init(&mutex, NULL))) {
     perror("pthread_mutex_init() failed");
     return EXIT_FAILURE;
   }
 
-  dpy = XOpenDisplay(NULL);
+  dpy    = XOpenDisplay(NULL);
   screen = DefaultScreen(dpy);
-  root = RootWindow(dpy, screen);
+  root   = RootWindow(dpy, screen);
 
   populate_with_initial_values();
 
